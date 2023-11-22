4cc3027c84aa645d4855bc55e1caa1720dfee893
arachatt <arachatt@LIN24007825.corp.capgemini.com>
2023-11-22 14:47:01 +0530

    Initial Commit

---
+++ darden/src/main/java/com/Darden/darden/SpringDardenApplication.java
@@ -0,0 +1,13 @@
+package com.Darden.darden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDardenApplication.class, args);
+	}
+
+}
---
+++ darden/src/test/java/com/Darden/darden/SpringDardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Darden.darden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDardenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
